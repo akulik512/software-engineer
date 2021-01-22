3b6704acbf1f2cbfab68a2d73f22f3a122031e37
Andrey Kulik <kulikav.96@gmail.com>
2021-01-22 20:58:05 +0300

    add a still one example

---
+++ design-patterns/src/main/java/com/akulik/designpatterns/command/GarageDoor.java
@@ -0,0 +1,15 @@
+package com.akulik.designpatterns.command;
+
+public class GarageDoor {
+    public void up() {
+        System.out.println("Garage Door is Up!");
+    }
+
+    public void down() {
+        System.out.println("Garage Door is Down!");
+    }
+
+    public void lightOn() {
+        System.out.println("Garage Door is Light On!");
+    }
+}
--- design-patterns/src/test/java/com/akulik/designpatterns/command/CommandTest.java
+++ design-patterns/src/test/java/com/akulik/designpatterns/command/CommandTest.java
@@ -9,10 +9,17 @@ class CommandTest {
     @Test
     void example() {
         final SimpleRemoteControl remote = new SimpleRemoteControl();
+
+        System.out.println("--- Light On ---");
         final Light light = new Light();
         final LightOnCommand lightOn = new LightOnCommand(light);
-
         remote.setSlot(lightOn);
+        remote.buttonWasPressed();
+
+        System.out.println("--- Garage Door ---");
+        final GarageDoor garageDoor = new GarageDoor();
+        final GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
+        remote.setSlot(garageDoorOpenCommand);
         remote.buttonWasPressed();
     }
 
