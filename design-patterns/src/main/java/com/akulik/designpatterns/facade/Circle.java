4291acd993dbc7c95d23cbde030bf30f55f0d856
Andrei Kulik <kulikav.96@gmail.com>
2021-01-29 00:21:24 +0300

    Design Pattern - Facade

---
+++ design-patterns/src/main/java/com/akulik/designpatterns/facade/Circle.java
@@ -0,0 +1,8 @@
+package com.akulik.designpatterns.facade;
+
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("I drew - Circle!");
+    }
+}
