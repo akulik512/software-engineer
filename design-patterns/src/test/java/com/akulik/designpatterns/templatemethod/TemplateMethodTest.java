90b5d768896f94247381373c4ab2eff881fed0dc
Andrei Kulik <kulikav.96@gmail.com>
2021-01-30 23:42:30 +0300

    Design Pattern - Template Method

---
+++ design-patterns/src/test/java/com/akulik/designpatterns/templatemethod/TemplateMethodTest.java
@@ -0,0 +1,21 @@
+package com.akulik.designpatterns.templatemethod;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Template Method")
+class TemplateMethodTest {
+
+    @Test
+    void example() {
+
+        System.out.println("Let's make some tea:");
+        final Tea tea = new Tea();
+        tea.prepareRecipe();
+
+        System.out.println("\nLet's make some coffee:");
+        final Coffee coffee = new Coffee();
+        coffee.prepareRecipe();
+    }
+
+}
