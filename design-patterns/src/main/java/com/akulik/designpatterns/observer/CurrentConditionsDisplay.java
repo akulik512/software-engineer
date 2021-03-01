f43584a96af59206dde5161a825f2981523387af
Andrey Kulik <kulikav.96@gmail.com>
2021-03-01 17:00:04 +0300

    Observer Refactoring: simplify CurrentConditionsDisplay

--- design-patterns/src/main/java/com/akulik/designpatterns/observer/CurrentConditionsDisplay.java
+++ design-patterns/src/main/java/com/akulik/designpatterns/observer/CurrentConditionsDisplay.java
@@ -2,23 +2,12 @@ package com.akulik.designpatterns.observer;
 
 public class CurrentConditionsDisplay implements Observer {
 
-    private float temp;
-    private float humidity;
-    private float pressure;
-
     public CurrentConditionsDisplay(Observable weatherData) {
         weatherData.registerObserver(this);
     }
 
     @Override
     public void update(float temp, float humidity, float pressure) {
-        this.temp = temp;
-        this.humidity = humidity;
-        this.pressure = pressure;
-        display();
-    }
-
-    public void display() {
         System.out.printf("Current conditions: %sF degrees and %s humidity and %shPa%n", temp, humidity, pressure);
     }
 
--- design-patterns/src/main/java/com/akulik/designpatterns/observer/WeatherData.java
+++ design-patterns/src/main/java/com/akulik/designpatterns/observer/WeatherData.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 
 public class WeatherData implements Observable {
 
-    private final ArrayList<Observer> observers;
+    private final ArrayList<Observer> observers = new ArrayList<>();
+
     private float temp;
     private float humidity;
     private float pressure;
-
-    public WeatherData() {
-        observers = new ArrayList<>();
-    }
 
     @Override
     public void registerObserver(Observer o) {
