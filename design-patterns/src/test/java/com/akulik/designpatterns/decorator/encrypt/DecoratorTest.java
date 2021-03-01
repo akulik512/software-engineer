439266b9b66d939fceccc67d13745b0d0c752ebd
Andrey Kulik <kulikav.96@gmail.com>
2021-03-01 17:13:22 +0300

    Decorator Refactoring: add output clarification messages

--- design-patterns/src/test/java/com/akulik/designpatterns/decorator/encrypt/DecoratorTest.java
+++ design-patterns/src/test/java/com/akulik/designpatterns/decorator/encrypt/DecoratorTest.java
@@ -9,8 +9,10 @@ class DecoratorTest {
     @Test
     void example() {
         final PublishDataSource publishDataSource = new PublishDataSource();
+        System.out.print("Original message: ");
         System.out.println(publishDataSource.printMessage("Important business feature!"));
 
+        System.out.print("Decorated message: ");
         final DataSourceDecorator encoded = new EncryptionDecorator(new PublishDataSource());
         System.out.println(encoded.printMessage("Important business feature!"));
     }
