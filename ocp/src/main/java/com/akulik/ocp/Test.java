62804cf2d1198bc15810fe8c32717f969536c73f
Andrey Kulik <kulikav.96@gmail.com>
2021-03-08 22:39:35 +0300

    operators =+ and +=

---
+++ ocp/src/main/java/com/akulik/ocp/Test.java
@@ -0,0 +1,16 @@
+package com.akulik.ocp;
+
+public class Test {
+    /**
+     * Operators =+ and +=
+    * */
+    public static void main(String[] args) {
+        int x = 1;
+        x =+ 2;
+
+        int y = 1;
+        y += 2;
+        System.out.println("x =+ 2 -> " + x);
+        System.out.println("y += 2 -> " + y);
+    }
+}
