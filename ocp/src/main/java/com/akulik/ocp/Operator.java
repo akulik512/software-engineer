5fbd6b836b652a79aed75df7caa47ae2094f2c3e
Andrey Kulik <kulikav.96@gmail.com>
2021-03-10 22:40:35 +0300

    add CompoundAssignments

--- ocp/src/main/java/com/akulik/ocp/Operator.java
+++ ocp/src/main/java/com/akulik/ocp/Operator.java
@@ -1,26 +1,45 @@
 package com.akulik.ocp;
 
 /**
- * 1 - Assignments and Arithmetics Operators
- * 2 -
+ * <pre>
+ * 1 - Assignments and Arithmetics Operators;
+ * 2 - Compound Assignments
+ * 3 -
+ * </pre>
  * */
 public class Operator {
 
     public static void main(String[] args) {
         // 1
         AssignmentsAndArithmetics arithmetics = new AssignmentsAndArithmetics();
-        arithmetics.result();
+        System.out.println(arithmetics.result());
 
+        // 2
+        CompoundAssignments assignments = new CompoundAssignments();
+        System.out.println(assignments.result());
     }
 
     static class AssignmentsAndArithmetics {
-        public void result() {
+        public int result() {
             int a = 1;      // assignment (a is 1)
             int b = a + 4;  // addition (b is 5)
             int c = b - 2;  // subtraction (c is 3)
             int d = c * b;  // multiplication (d is 15)
             int e = d / c;  // division (e is 5)
             int f = e % 6;  // modulus (f is 3)
+            return f;
+        }
+    }
+
+    static class CompoundAssignments {
+        public int result() {
+            int a = 1, b = 3;
+            a += b;      // equivalent of a = a + b; (a is 4)
+            a -= 2;      // equivalent of a = a - 2; (a is 2)
+            a *= b;      // equivalent of a = a * 6; (a is 6)
+            a /= 2;      // equivalent of a = a / 2; (a is 3)
+            a %= a;      // equivalent of a = a / 2; (a is 0)
+            return a;
         }
     }
 }
