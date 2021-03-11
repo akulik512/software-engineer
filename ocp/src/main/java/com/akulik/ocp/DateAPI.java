96fe99f988d4c4901a85cf87d0a90c00d6ddfbb9
Andrey Kulik <kulikav.96@gmail.com>
2021-03-12 00:59:32 +0300

    rename DateAPI.java from InstantsDurationsPeriods.java

---
+++ ocp/src/main/java/com/akulik/ocp/DateAPI.java
@@ -0,0 +1,33 @@
+package com.akulik.ocp;
+
+import java.time.*;
+
+public class DateAPI {
+
+    public static void main(String[] args) {
+        // 1
+        InstantsDurationsPeriods instantsDurationsPeriods = new InstantsDurationsPeriods();
+        instantsDurationsPeriods.result();
+    }
+
+    private static class InstantsDurationsPeriods {
+        private void result() {
+            LocalDate today = LocalDate.now();
+            LocalDate foolsDay = LocalDate.of(2019, Month.APRIL,1);
+
+            Instant timeStamp = Instant.now();
+            int nanoSecondsFromLastSecond = timeStamp.getNano();
+            System.out.println(nanoSecondsFromLastSecond); // 331 938 000
+
+            Period howLong = Period.between(foolsDay, today);
+            System.out.println(howLong); // P 1Y 11M 11D
+
+            Duration twoHours = Duration.ofHours(2);
+            System.out.println(twoHours); // PT2H
+
+            long seconds = twoHours.minusMinutes(15).getSeconds();
+            int days = howLong.getDays();
+            System.out.println(seconds + " " + days); // 6300 11
+        }
+    }
+}
