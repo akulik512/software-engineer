1ee9a9c35ad1363d256125ca7847bd79dc0484ef
Andrey Kulik <kulikav.96@gmail.com>
2021-03-12 01:04:57 +0300

    refactoring

--- ocp/src/main/java/com/akulik/ocp/DateAPI.java
+++ ocp/src/main/java/com/akulik/ocp/DateAPI.java
@@ -6,32 +6,32 @@ public class DateAPI {
 
     public static void main(String[] args) {
         // 1
-        InstantsDurationsPeriods instantsDurationsPeriods = new InstantsDurationsPeriods();
-        instantsDurationsPeriods.result();
+        DurationAPI durationAPI = new DurationAPI();
+        durationAPI.result();
 
         // 2
         ZoneAPI zoneAPI = new ZoneAPI();
         zoneAPI.result();
     }
 
-    private static class InstantsDurationsPeriods {
+    private static class DurationAPI {
         private void result() {
             LocalDate today = LocalDate.now();
             LocalDate foolsDay = LocalDate.of(2019, Month.APRIL,1);
 
             Instant timeStamp = Instant.now();
             int nanoSecondsFromLastSecond = timeStamp.getNano();
-            System.out.println(nanoSecondsFromLastSecond); // 331 938 000
+            System.out.println(nanoSecondsFromLastSecond);  // 331 938 000
 
             Period howLong = Period.between(foolsDay, today);
-            System.out.println(howLong); // P 1Y 11M 11D
+            System.out.println(howLong);                    // P 1Y 11M 11D
 
             Duration twoHours = Duration.ofHours(2);
-            System.out.println(twoHours); // PT2H
+            System.out.println(twoHours);                   // PT2H
 
             long seconds = twoHours.minusMinutes(15).getSeconds();
             int days = howLong.getDays();
-            System.out.println(seconds + " " + days); // 6300 11
+            System.out.println(seconds + " " + days);       // 6300 11
         }
     }
 
