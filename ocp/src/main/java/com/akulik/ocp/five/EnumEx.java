d6498257b813f1f1c364b205b9eef8e328b86b81
Andrey Kulik <kulikav.96@gmail.com>
2021-03-14 00:16:09 +0300

    add EnumEx.java

---
+++ ocp/src/main/java/com/akulik/ocp/five/EnumEx.java
@@ -0,0 +1,11 @@
+package com.akulik.ocp.five;
+
+public enum EnumEx {
+    // We must create parametrized constructor
+    HOT("Hello, World!");
+
+    // Only private or default
+    EnumEx(String message) {
+
+    }
+}
