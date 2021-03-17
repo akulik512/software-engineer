2cfe4d76eaffce1e6632864cb4b4e708f57c6778
Andrey Kulik <kulikav.96@gmail.com>
2021-03-17 23:07:23 +0300

    array

--- ocp/src/main/java/com/akulik/ocp/Array.java
+++ ocp/src/main/java/com/akulik/ocp/Array.java
@@ -1,19 +1,15 @@
 package com.akulik.ocp;
 
-import java.util.Arrays;
-
 public class Array {
 
-    /**
-     *
-     * <link>
-     *     https://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html#fill(java.lang.Object[],%20java.lang.Object)
-     * </link>
-     */
     public static void main(String[] args) {
-        String[] array = new String[5];
-        Arrays.fill(array, "Hello");
-        System.out.println(array[2]);
+        String[] arr = new String[5];
+        System.out.println("Length = " + arr.length);
+        arr[0] = "Hello!";
+        System.out.println("Last valid position = " + (arr.length - 1));
+
+        int[][] matrix = {{4, 1}, {2, 0, 5}};
+        System.out.println(matrix[1][1]);
     }
 
 }
