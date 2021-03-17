77fae391f3f3b0d91de8406be2b117ae0539e627
Andrey Kulik <kulikav.96@gmail.com>
2021-03-18 00:02:23 +0300

    add Loop.java

--- ocp/src/main/java/com/akulik/ocp/eight/Array.java
+++ ocp/src/main/java/com/akulik/ocp/eight/Array.java
@@ -4,7 +4,7 @@ package com.akulik.ocp.eight;
  * 8: Arrays and Loops
  */
 public class Array {
-
+    // Part 1
     public static void main(String[] args) {
         String[] arr = new String[5];
         System.out.println("Length = " + arr.length);
@@ -12,7 +12,12 @@ public class Array {
         System.out.println("Last valid position = " + (arr.length - 1));
 
         int[][] matrix = {{4, 1}, {2, 0, 5}};
-        System.out.println(matrix[1][1]);
+        System.out.println(matrix[1][2]); // 5
+
+        char[][] game = {{'O', 'X', 'X'},
+                         {'X', 'X', 'O'},
+                         {'O', ' ', 'O'}};
+        System.out.println(game.length);
     }
 
 }
