fcf75cd4d83403765e222a3a9ade33ea9aec1228
Andrey Kulik <kulikav.96@gmail.com>
2021-03-19 02:40:20 +0300

    add MapEx.java

---
+++ ocp/src/main/java/com/akulik/ocp/nine/MapEx.java
@@ -0,0 +1,22 @@
+package com.akulik.ocp.nine;
+
+import java.util.HashMap;
+
+public class MapEx {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Hello");
+        map.put(2, "World");
+        map.put(3, null);
+        System.out.println("Original map: " + map);
+
+        map.putIfAbsent(4, "Kulik");
+        System.out.println("1 Updated map: " + map);
+
+        map.putIfAbsent(3, "Andrei");
+        System.out.println("2 Updated map: " + map);
+
+        map.putIfAbsent(3, "Vasya");
+        System.out.println("3 Updated map: " + map);
+    }
+}
