7197b4178bfc8511336a2b19ead94ff0744872ef
Andrey Kulik <kulikav.96@gmail.com>
2021-03-18 22:42:28 +0300

    add ArrayListCreationDiff

--- ocp/src/main/java/com/akulik/ocp/nine/CollectionsEx.java
+++ ocp/src/main/java/com/akulik/ocp/nine/CollectionsEx.java
@@ -27,6 +27,67 @@ public class CollectionsEx {
             List<String> stringList = Arrays.asList("string");
             // stringList.add("new string");
             // System.out.println(stringList);// EXCEPTION!
+
+            // Difference between List.of and Arrays.asList?
+            ArrayListCreationDiff arrayListCreationDiff = new ArrayListCreationDiff();
+            arrayListCreationDiff.result();;
+        }
+
+        private static class ArrayListCreationDiff {
+            private void result() {
+                showImmutable();
+                showNull();
+                showContains();
+                showReflectedChanges();
+            }
+
+            private void showImmutable() {
+                System.out.println("\n-- 1");
+
+                List<Integer> asList = Arrays.asList(1, 2, null);
+                System.out.println("Original asList: " + asList);
+                asList.set(1, 10); // OK
+                System.out.println("Modified asList: " + asList);
+
+                List<Integer> of = List.of(1, 2, 3);
+                System.out.println("Original of: " + of);
+                // System.out.println("Modified of: " + of.set(1, 10)); // Fails with UnsupportedOperationException
+            }
+
+            private void showNull() {
+                System.out.println("\n-- 2");
+
+                List<Integer> asList = Arrays.asList(1, 2, null); // OK
+                System.out.println("Original asList: " + asList);
+
+                // List<Integer> of = List.of(1, 2, null); // Fails with NullPointerException
+            }
+
+            private void showContains() {
+                System.out.println("\n-- 3");
+
+                List<Integer> asList = Arrays.asList(1, 2, 3);
+                System.out.println("Original asList: " + asList.contains(null)); // Returns false
+
+                List<Integer> of = List.of(1, 2, 3);
+                // of.contains(null); // Fails with NullPointerException
+            }
+
+            private void showReflectedChanges() {
+                System.out.println("\n-- 4");
+
+                Integer[] array1 = {1,2,3};
+                List<Integer> asList = Arrays.asList(array1);
+                System.out.println("Original asList: " + asList);
+                array1[1] = 10;
+                System.out.println("Modified asList: " + asList); // Prints [1, 10, 3]
+
+                Integer[] array2 = {1,2,3};
+                List<Integer> of = List.of(array2);
+                System.out.println("Original of: " + of);
+                array2[1] = 10;
+                System.out.println("Modified of: " + of); // Prints [1, 2, 3]
+            }
         }
     }
 }
