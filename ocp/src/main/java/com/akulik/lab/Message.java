4eb576daab56533fcfd14ad0111bc0c949a99e3b
Andrey Kulik <kulikav.96@gmail.com>
2021-03-20 21:59:16 +0300

    Comparator example

---
+++ ocp/src/main/java/com/akulik/lab/Message.java
@@ -0,0 +1,27 @@
+package com.akulik.lab;
+
+public class Message {
+    private int id;
+    private String msg;
+
+    public Message(int id, String msg) {
+        this.id = id;
+        this.msg = msg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
