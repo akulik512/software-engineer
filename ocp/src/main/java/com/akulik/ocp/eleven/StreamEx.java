fe4b639848eaba45dd6ffb44fabbee8247c0be6a
Andrey Kulik <kulikav.96@gmail.com>
2021-03-21 18:54:44 +0300

    add benchmark

--- ocp/src/main/java/com/akulik/ocp/eleven/StreamEx.java
+++ ocp/src/main/java/com/akulik/ocp/eleven/StreamEx.java
@@ -1,5 +1,13 @@
 package com.akulik.ocp.eleven;
 
+import com.akulik.lab.Message;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamEx {
@@ -11,5 +19,35 @@ public class StreamEx {
                 .sum();
 
         System.out.println("Sum=" + sum);
+
+        parallelStreamTest();
+        sequentialStreamTest();
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public static void parallelStreamTest() {
+        List<Message> names = new ArrayList<>();
+        names.add(new Message(1, "Brest"));
+        names.add(new Message(1, "Minsk"));
+        names.add(new Message(1, "Grodno"));
+
+        names.stream()
+                .parallel()
+                .map(Message::getId)
+                .collect(Collectors.toList());
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    public static void sequentialStreamTest() {
+        List<Message> names = new ArrayList<>();
+        names.add(new Message(1, "Brest"));
+        names.add(new Message(1, "Minsk"));
+        names.add(new Message(1, "Grodno"));
+
+        names.stream()
+                .map(Message::getId)
+                .collect(Collectors.toList());
     }
 }
