eaf38b2b53811afd682d198e124250706ebc0511
Andrey Kulik <kulikav.96@gmail.com>
2021-03-22 20:18:39 +0300

    rename ConnectingStreams.java to JavaIO.java

---
+++ ocp/src/main/java/com/akulik/ocp/thirteen/JavaIO.java
@@ -0,0 +1,31 @@
+package com.akulik.ocp.thirteen;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class JavaIO {
+    private static final String PATH = "/home/akulik/IdeaProjects/software-engineer/ocp/src/main/java/com/akulik/ocp/thirteen/";
+
+    public static void main(String[] args) {
+        // 1
+        ConnectingStream connectingStream = new ConnectingStream();
+        connectingStream.result();
+    }
+
+    private static class ConnectingStream {
+        private void result() {
+            Charset utf8 = StandardCharsets.UTF_8;
+
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(String.format("%sthirteen_in.txt", PATH)), utf8));
+                 PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(String.format("%sthirteen_out.txt", PATH)), utf8))) {
+                String line = null;
+                while ((line = in.readLine()) != null) {
+                    out.write(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
