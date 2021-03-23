1b036edbb741547bd654dc88891e5bfb5ecc4d0e
Andrey Kulik <kulikav.96@gmail.com>
2021-03-23 19:33:17 +0300

    add part three for two module

--- ocp/src/main/java/com/akulik/ocp/two/PrimitiveTypes.java
+++ ocp/src/main/java/com/akulik/ocp/two/PrimitiveTypes.java
@@ -3,82 +3,11 @@ package com.akulik.ocp.two;
 public class PrimitiveTypes {
 
     public static void main(String[] args) {
-        // 5
         PrimitiveTypesPart1 partOne = new PrimitiveTypesPart1();
         partOne.result();
 
-        // 5
-        ConditionalOperators conditionalOperators = new ConditionalOperators();
-        System.out.println(conditionalOperators.result());
-
-        // 6
-        ShortCircuitEvaluation shortCircuitEvaluation = new ShortCircuitEvaluation();
-        System.out.println(shortCircuitEvaluation.result());
-    }
-
-    private static class ConditionalOperators {
-        private boolean result() {
-            int a = 3, b = 2;
-            boolean c = false;
-
-            c = (a == b);           // c is false
-            c = !(a == b);          // c is true
-            c = (a != b);           // c is false
-            c = (a > b);            // c is true
-            c = (a >= b);           // c is true
-            c = (a < b);            // c is false
-            c = (a <= b);           // c is false
-
-            c = (a > b && b == 2);  // c is true
-            c = (a > b && b == 2);  // c is true
-            c = (a < b && b == 2);  // c is false
-            c = (a < b || b == 2);  // c is true
-            c = (a < b || b == 3);  // c is false
-
-            // For Exclusive OR, either part has to be true,
-            // and the other part has to be false for the overall result to become true.
-            c = (a > b ^ b == 2);   // c is false
-            return c;
-        }
-    }
-
-    /*
-     *
-     * 1 - Short-Circuit Evaluation && ||
-     * 2 - Full Evaluation & |
-     *
-     * Short-circuit evaluation enables you to not evaluate the right-hand side of the AND and OR expressions
-     * when the overall result can be predicted from the left-hand side value.
-     *
-     * true     &&  evaluated
-     * false    &&  not evaluated
-     * false    &   evaluated
-     * false    ||  evaluated
-     * true     ||  not evaluated
-     * true     |   evaluated
-     * true     ^   evaluated
-     * false    ^   evaluated
-     *
-     * Good example:
-     * if (foo() || bar() || baz()) {
-     *      // Do something
-     * }
-     * If foo() returns true, then bar and baz are never called, because the expression's outcome
-     * has already been determined. So if bar or baz has some other effect than just returning something (a side effect),
-     * those effects never occur.
-     * */
-    private static class ShortCircuitEvaluation {
-        private boolean result() {
-            int a = 3, b = 2;
-            boolean c = false;
-
-            c = (a > 2 && ++b == 3);    // c is true, b is 3
-            c = (a > 2 && ++b == 3);    // c is false, b is 3
-            c = (a > 2 || ++b == 3);    // c is false, b is 4
-            c = (a > 2 || ++b == 3);    // c is false, b is 4
-            c = (a > 2 | ++b == 3);     // c is false, b is 5
-            return c;
-        }
+        PrimitiveTypesPart3 partThree = new PrimitiveTypesPart3();
+        partThree.result();
     }
 
 }
