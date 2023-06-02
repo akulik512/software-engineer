3d1fdca00965d4e2b754ca39e3d8eaf4bd16da94
Andrei Kulik <kulikav.96@gmail.com>
2023-06-03 00:27:28 +0300

    Use case: refactoring

--- software-engineer-leetcode/build.gradle.kts
+++ software-engineer-leetcode/build.gradle.kts
@@ -13,6 +13,12 @@ dependencies {
     testRuntimeOnly(libs.junit.jupiter.engine)
     testImplementation(libs.junit.jupiter.params)
     testImplementation(libs.assertj.core)
+
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.28")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
 }
 
 tasks.getByName<Test>("test") {
---
+++ software-engineer-leetcode/src/main/java/com/akulik/usecase/Book.java
@@ -0,0 +1,12 @@
+package com.akulik.usecase;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Book {
+
+    private final String title;
+
+}
