72ae55f1fbc7349e5b62b4127c548fad04437bf7
Andrei Kulik <kulikav.96@gmail.com>
2024-02-11 21:50:39 +0200

    BinarySearch and Hastable

--- software-engineer-leetcode/src/main/java/com/akulik/algorithm/BinarySearch.java
+++ software-engineer-leetcode/src/main/java/com/akulik/algorithm/BinarySearch.java
@@ -2,7 +2,7 @@ package com.akulik.algorithm;
 
 public class BinarySearch {
 
-    public int binarySearch(int[] nums, int target) {
+    public static int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
 
@@ -13,6 +13,19 @@ public class BinarySearch {
             if (target > nums[middle]) left = middle + 1;
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int targetElement = 7;
+
+        int result = binarySearch(sortedArray, targetElement);
+
+        if (result != -1) {
+            System.out.println("Element " + targetElement + " found at index " + result);
+        } else {
+            System.out.println("Element " + targetElement + " not found in the array.");
+        }
     }
 
 }
---
+++ software-engineer-leetcode/src/main/java/com/akulik/hashtable/HashTable.java
@@ -0,0 +1,84 @@
+package com.akulik.hashtable;
+
+import java.util.LinkedList;
+
+class HashTable {
+
+    private static final int SIZE = 10;
+    private final LinkedList<Entry>[] table;
+
+    public HashTable() {
+        table = new LinkedList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
+
+    public static void main(String[] args) {
+        HashTable hashtable = new HashTable();
+
+        hashtable.put("key1", "value1");
+        hashtable.put("key2", "value2");
+        hashtable.put("key3", "value3");
+
+        System.out.println("Value for key1: " + hashtable.get("key1"));
+        System.out.println("Value for key2: " + hashtable.get("key2"));
+        System.out.println("Value for key3: " + hashtable.get("key3"));
+        System.out.println("Value for non-existent key: " + hashtable.get("nonexistent"));
+
+        System.out.println("Does algorithm.hashtable contain key2? " + hashtable.containsKey("key2"));
+        System.out.println("Does algorithm.hashtable contain nonexistent key? " + hashtable.containsKey("nonexistent"));
+    }
+
+    private int hashFunction(String key) {
+        // Simple hash function for demonstration purposes
+        return key.hashCode() % SIZE;
+    }
+
+    public void put(String key, String value) {
+        int index = hashFunction(key);
+        LinkedList<Entry> list = table[index];
+
+        for (Entry entry : list) {
+            if (entry.key.equals(key)) {
+                // Key already exists, update the value
+                entry.value = value;
+                return;
+            }
+        }
+
+        // Key doesn't exist, add a new entry
+        list.add(new Entry(key, value));
+    }
+
+    public String get(String key) {
+        int index = hashFunction(key);
+        LinkedList<Entry> list = table[index];
+
+        for (Entry entry : list) {
+            if (entry.key.equals(key)) {
+                // Key found, return the associated value
+                return entry.value;
+            }
+        }
+
+        // Key not found
+        return null;
+    }
+
+    public boolean containsKey(String key) {
+        int index = hashFunction(key);
+        LinkedList<Entry> list = table[index];
+
+        for (Entry entry : list) {
+            if (entry.key.equals(key)) {
+                // Key found
+                return true;
+            }
+        }
+
+        // Key not found
+        return false;
+    }
+
+}
