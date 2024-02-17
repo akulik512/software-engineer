f78935db8d572ce82b152c4d11cae55980088140
Andrei Kulik <kulikav.96@gmail.com>
2024-02-17 22:03:22 +0200

    queue refactoring

--- src/main/java/core/queue/PriorityQueueDemo.java
+++ src/main/java/core/queue/PriorityQueueDemo.java
@@ -8,27 +8,35 @@ import java.util.TreeSet;
 
 public class PriorityQueueDemo {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    //creating priorityQueue
+    //default capacity = 11
+    PriorityQueue pq = new PriorityQueue();
 
-        //creating priorityQueue
-        PriorityQueue pq = new PriorityQueue();
-        //defaul size = 11
-        //DNSO
+    //creating priorityQueue with specified size
+    PriorityQueue pq1 = new PriorityQueue(20);
 
-        //creating priorityQueue with specified size
-        PriorityQueue pq1 = new PriorityQueue(20);
+    //creating priorityQueue with initial capacity and comparator object
+    //PriorityQueue pq2 = new PriorityQueue(int initialCapacity,Comparator c);
 
-        //creating priorityQueue with initial capacity and comparator object
-        //PriorityQueue pq2 = new PriorityQueue(int initialCapacity,Comparator c);
+    SortedSet s = new TreeSet();
+    //creating priorityQueue equivalent to sortedSet s
+    PriorityQueue pq3 = new PriorityQueue(s);
 
-        SortedSet s = new TreeSet();
-        //creating priorityQueue equivalent to sortedSet s
-        PriorityQueue pq3 = new PriorityQueue(s);
+    List l = new ArrayList();
+    //creating equivalent priorityQueue for any collection
+    PriorityQueue pq4 = new PriorityQueue(l);
 
-        List l = new ArrayList();
-        //creating equivalent priorityQueue for any collection
-        PriorityQueue pq4 = new PriorityQueue(l);
+    System.out.println("Test duplicates");
+    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    priorityQueue.add(3);
+    priorityQueue.add(1);
+    priorityQueue.add(2);
+    priorityQueue.add(1); // Duplicate
 
+    while (!priorityQueue.isEmpty()) {
+      System.out.println(priorityQueue.poll());
     }
+  }
 
 }
