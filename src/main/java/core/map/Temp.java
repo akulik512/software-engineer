cd9f7dbfc2b0b7fb08395c74e0a618a525cf4a9e
Andrei Kulik <kulikav.96@gmail.com>
2024-02-17 13:15:39 +0200

    add the Map section

---
+++ src/main/java/core/map/Temp.java
@@ -0,0 +1,13 @@
+package core.map;
+
+public class Temp {
+
+    public String toString() {
+        return "temp";
+    }
+
+    protected void finalize() {
+        System.out.println("Finalize method called");
+    }
+
+}
